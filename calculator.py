826f212c87b4afa8d983f4129ae4e1a19f8b8f0d
iamaquaman <iamaquaman@outlook.com>
2018-09-29 21:13:26 -0700

    Calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+while True:
+    print("Options:")
+    print("Enter 'add' to substract two numbers")
+    print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to multiply two numbers")
+    print("Enter 'divide' to divide two numbers")
+    print("Enter 'quit' to end the program")
+
+    if user_input == "quit":
+        break
+    elif user_input == "add":
+    elif user_input == "subtract":
+    elif user_input == "multiply":
+    elif user_input == "divide":
+    else:
+        print("Unknown input")
+    
+    
+
+
