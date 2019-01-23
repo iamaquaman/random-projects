e6bd62b78660f8a224c5e41b2be4da248b151ccc
Jonathan Muralles <iamaquaman@outlook.com>
2019-01-22 16:57:43 -0800

    datadog logger sample

---
+++ logger.go
@@ -0,0 +1,25 @@
+// This is a sample Data-Dog sample logger
+
+package main
+
+import (
+  log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+
+    // use JSONFormatter
+    log.SetFormatter(&log.JSONFormatter{})
+
+    // log an event as usual with logrus
+    log.WithFields(log.Fields{"string": "foo", "int": 1, "float": 1.1 }).Info("My first event from golang to stdout")
+  // For metadata, a common pattern is to re-use fields between logging statements  by re-using
+  contextualizedLog := log.WithFields(log.Fields{
+    "hostname": "staging-1",
+    "appname": "foo-app",
+    "session": "1ce3f6v"
+  })
+
+  contextualizedLog.Info("Simple event with global metadata")
+  
+}
